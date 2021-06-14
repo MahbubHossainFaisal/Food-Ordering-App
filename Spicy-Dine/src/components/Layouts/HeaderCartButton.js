b3d4b4327f814a8d48c63a21e36f3b5760450f50
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-06-15 00:13:24 +0600

    Using useEffect to give cart button a bump effect

--- Spicy-Dine/src/components/Layouts/HeaderCartButton.js
+++ Spicy-Dine/src/components/Layouts/HeaderCartButton.js
@@ -1,16 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context'
 import classes from './HeaderCartButton.module.css'
 const HeaderCartButton = (props) => {
     const cartCtx = useContext(CartContext)
-
-    const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+    const [btnIsHighlighted, setBtnIsHighLighted] = useState(false)
+    const { items } = cartCtx
+    const numberOfCartItems = items.reduce((currNumber, item) => {
         return currNumber + item.amount
     }, 0)
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return
+        }
+        setBtnIsHighLighted(true)
+        const timer = setTimeout(() => {
+            setBtnIsHighLighted(false)
+        }, 300)
+
+        return(()=>{
+            clearTimeout(timer)
+        })
+    }, [items])
+
+
     return (
         <div>
-            <button className={classes.button} onClick={props.onCartClick}>
+            <button className={btnClasses} onClick={props.onCartClick}>
                 <span className={classes.icon}>
                     <CartIcon />
                 </span>
